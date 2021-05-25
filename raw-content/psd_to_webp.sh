12a33934477b3070cb1efad05dd2fc5c4aa1df86
Wisdman <wisdman@wisdman.io>
2021-05-26 01:03:13 +0500

    Add carousel animations

---
+++ raw-content/psd_to_webp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+find ${PWD} -type f -iname "*.webp" -delete
+find ${PWD} -type f -iname "*.psd" -execdir sh -c 'magick "${0%}[0]" -resize 1920x1080 -define webp:lossless=true ${0%.*}.webp' {} ';'
