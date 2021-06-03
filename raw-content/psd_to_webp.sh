edf8f76fce5736252cb2aabf327cef154a6226db
Wisdman <wisdman@wisdman.io>
2021-06-03 12:09:40 +0500

    Update content

--- raw-content/psd_to_webp.sh
+++ raw-content/psd_to_webp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 find ${PWD} -type f -iname "*.webp" -delete
-find ${PWD} -type f -iname "*.psd" -execdir sh -c 'magick "${0%}[0]" -resize 1920x1080 -define webp:lossless=true ${0%.*}.webp' {} ';'
+find ${PWD} -type f -iname "*.psd" -execdir sh -c 'magick "${0%}[0]" -resize 1920x1080 -define webp:lossless=true "${0%.*}.webp"' {} ';'
